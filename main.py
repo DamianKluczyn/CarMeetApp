4b1c0f4f2b8c727a7b91fff29dee1c4748f8ea04
Damian Kluczyński <damiankluczynski00@gmail.com>
2023-01-13 17:47:45 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import psycopg2
+import bcrypt
+from abc import ABC, abstractmethod
+
+class Database_connection:
+    pass
+
+class Proxy:
+    pass
+
+class Login:
+    pass
+
+class Register:
+    pass
+
+class Hash:
+    pass
+
+class Logged_user:
+    pass
+
+class User_exist:
+    pass
+
+class IFlyweight(ABC):
+    pass
+
+class Flyweight(IFlyweight):
+    pass
+
+class Flyweight_factory:
+    pass
+
+class Meet_state:
+    pass
+
+class No_meet_state:
+    pass
+
+class Login_menu:
+    pass
+
+class Logged_menu:
+    pass

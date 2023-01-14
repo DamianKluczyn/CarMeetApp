1b47bb5cf09a2cf0963f47d955448b7629bd098a
Damian Kluczyński <damiankluczynski00@gmail.com>
2023-01-14 01:46:51 +0100

    Added register proxy, password, nick and user check

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import psycopg2
 import bcrypt
+import re
+import os
 from abc import ABC, abstractmethod
 from typing import Dict
 
+
+
+# Interface's
 class User(ABC):
     @abstractmethod
     def get_state(self):
         pass
+
+class State(ABC):
+    @abstractmethod
+    def change_state(self):
+        pass
+
+# Database Connection
 
 class Database_connection:
     def __init__(self):
@@ -17,7 +29,11 @@ class Database_connection:
         )
         self.cur = self.con.cursor()
 
-class User_exist:
+
+# Functional classes
+
+# Check if user with this login already exists
+class User_exist(Database_connection):
     def __init__(self, login):
         super().__init__()
         self._login = login
@@ -31,10 +47,53 @@ class User_exist:
         # user doesn't exist
         return False
 
+# Check if user with this nick already exists
+class Nick_exist(Database_connection):
+    def __init__(self, nick):
+        super().__init__()
+        self._nick = nick
+
+    def check(self) -> bool:
+        self.cur.execute('select count(*) from "Account" where nick = %s;', (self.nick,))
+        in_base = self.cur.fetchone()
+        if (in_base[0] == 1):
+            # nick exists
+            return True
+        # nick doesn't exist
+        return False
+
+# Check if password is strong (at least 8 letters, 1 capital, 1 number, 1 special char)
+class Password_check():
+    def check(self, password):
+        if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+            return True
+        return False
+
+# Hashes password using bcrypt algorithm
+class Hash:
+    def __init__(self, password) -> None:
+        self._password = password.encode('utf-8')
+        self._salt = bcrypt.gensalt()
+        self._hashed_password = bcrypt.hashpw(self._password, self._salt).decode('utf-8')
+
+    def get_hashed_password(self) -> str:
+        return self._hashed_password
+
+
+# States (Meet - wants to meet, No_meet - closed for meetings/offline
+class Meet_state(State):
+    def change_state(self):
+        pass
+
+class No_meet_state(State):
+    def change_state(self):
+        pass
+
+# Login user from database
 class Login(Database_connection):
-    def __init__(self):
+    def __init__(self) -> None:
         self._login = ""
-        self._passw = ""
+        self._password = ""
         super().__init__()
 
     def getLogin(self) -> str:
@@ -43,45 +102,33 @@ class Login(Database_connection):
     def sign(self) -> bool:
         self.cur.execute('select password from "Account" where login = %s;', (self._login,))
         rows = self.cur.fetchone()
-        if (bcrypt.checkpw(self._passw.encode('utf-8'), rows[0].encode('utf-8')) == True):
+        if (bcrypt.checkpw(self._password.encode('utf-8'), rows[0].encode('utf-8')) == True):
             # login successfully
             return True
         # login failed
         return False
 
-class Register:
-    def __init__(self):
+
+# Register user in database
+class Register(Database_connection):
+    def __init__(self) -> None:
         self._login, self._password, self._nick, self._state = "", "", "", No_meet_state
         #TODO zmiana parametrow
         super().__init__()
 
-    def register(self, login, password, nick) -> bool:
+    def register(self, login, password, nick) -> None:
         self._login = login
         self._password = password
         self._nick = nick
-        #TODO przejscie przez proxy
-        if (self._exist.check()):
-            self._passw = hash(self._passw)
-            self.cur.execute('insert into "Account" (login, password, fname, lname) values (%s, %s, %s, %s);',
-                             (self._login, self._passw.get_hashed_password(), self._fname, self._lname))
-            self.con.commit()
-            print("\nUser registered succesfully!\n")
-            return True
-        print("\nRegistration failed!\n")
-        return False
+        self.cur.execute('insert into "Account" (login, password, nick, state) values (%s, %s, %s, "no_meet_state");',
+                            (self._login, self._passw.get_hashed_password(), self._fname, self._lname))
+        self.con.commit()
 
-class Hash:
-    def __init__(self, password):
-        self._password = password.encode('utf-8')
-        self._salt = bcrypt.gensalt()
-        self._hashed_password = bcrypt.hashpw(self._password, self._salt).decode('utf-8')
-
-    def get_hashed_password(self) -> str:
-        return self._hashed_password
 
 class Logged_user(User):
-    def __init__(self, login):
+    def __init__(self, login, state: State):
         self._login = login
+        self._state = state
 
     def get_login(self) -> str:
         return self._login
@@ -142,7 +189,27 @@ class Proxy(User):
     def login_proxy(self):
         pass
     def register_proxy(self):
-        pass
+        while(True):
+            login = str(input("Login: "))
+            password = str(input("Password: "))
+            nick = str(input("Nick: "))
+            if(not User_exist(login).check()):
+                if(not Nick_exist(nick).check()):
+                    if(Password_check.check(password)):
+                        password = Hash(password).get_hashed_password()
+                        Register.register(login, password, nick)
+                        print("Registered successfully!")
+                        break;
+                    else:
+                        print("Register failed!\nPassword too weak! - use at least 8 letters, 1 capital, 1 number, 1 special char.")
+                else:
+                    print("Register failed!\nNick already exists!")
+            else:
+                print("Register failed!\n User with this login already exists")
+            input("Press Enter to continue...")
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+
     def add_car_proxy(self):
         # TODO flyweight
         pass
@@ -168,19 +235,6 @@ class Proxy(User):
     def getState(self) -> str:
         return self._real_subject.getState()
     '''
-
-class State(ABC):
-    @abstractmethod
-    def change_state(self):
-        pass
-
-class Meet_state(State):
-    def change_state(self):
-        pass
-
-class No_meet_state(State):
-    def change_state(self):
-        pass
 
 class Login_menu:
     pass

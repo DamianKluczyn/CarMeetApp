0e8c42c0619442fdf2c8fd919a5183bd4abffdb4
Damian Kluczyński <damiankluczynski00@gmail.com>
2023-01-15 17:08:39 +0100

    Added login, register menu

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ class State(ABC):
     def change_state(self):
         pass
 
+class Logged_user:
+    pass
+
 
 # Database Connection - singleton
 
@@ -48,7 +51,7 @@ class User_exist():
         self._db = db
 
     def check(self) -> bool:
-        self._db.cur.execute('select count(*) from "Account" where login = %s;', (self.login,))
+        self._db.cur.execute('select count(*) from "Account" where login = %s;', (self._login,))
         in_base = self._db.cur.fetchone()
         if (in_base[0] == 1):
             # user exists
@@ -63,7 +66,7 @@ class Nick_exist():
         self._db = db
 
     def check(self) -> bool:
-        self._db.cur.execute('select count(*) from "Account" where nick = %s;', (self.nick,))
+        self._db.cur.execute('select count(*) from "Account" where nick = %s;', (self._nick,))
         in_base = self._db.cur.fetchone()
         if (in_base[0] == 1):
             # nick exists
@@ -74,12 +77,12 @@ class Nick_exist():
 # Check if password is strong (at least 8 letters, 1 capital, 1 number, 1 special char)
 class Password_check():
     def check(self, password: str) -> bool:
-        if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+        if re.fullmatch(r'[A-Za-z0-9@#$%^&+!*()_\-=]{8,}', password):
             return True
         return False
 
 # Hashes password using bcrypt algorithm
-class Hash:
+class Hash():
     def __init__(self, password: str) -> None:
         self._password = password.encode('utf-8')
         self._salt = bcrypt.gensalt()
@@ -91,6 +94,20 @@ class Hash:
 def Clear_terminal() -> None:
     input("\nPress Enter to continue...")
     os.system('cls' if os.name == 'nt' else 'clear')
+
+class HasCar():
+    def __init__(self, login: str, db: Database_connection) -> None:
+        self._login = login
+        self._db = db
+
+    def check(self) -> bool:
+        self._db.cur.execute('select id_car from "Account" where login = %s;', (self._login,))
+        in_base = self._db.cur.fetchone()
+        if (in_base[0] == None):
+            # doesn't have car
+            return False
+        # has car
+        return True
 
 
 # States (Meet - wants to meet, No_meet - closed for meetings/offline
@@ -115,10 +132,10 @@ class Login(User):
     def get_login(self) -> str:
         return self._login
 
-    def get_state(self, state: str) -> State:
+    def get_state(self) -> State:
         self._db.cur.execute('select state from "Account" where login = %s;', (self._login,))
         rows = self._db.cur.fetchone()
-        if (rows[0] == "no_meet_state"):
+        if (rows[0] == False):
             return No_meet_state()
         return Meet_state()
 
@@ -134,22 +151,23 @@ class Login(User):
 # Register user in database
 class Register():
     def __init__(self, db: Database_connection) -> None:
-        self._login, self._password, self._nick, self._state = "", "", "", "no_meet_state"
+        self._login, self._password, self._nick, self._state = "", "", "", False
         self._db = db
 
     def register(self, login: str, password: str, nick: str) -> None:
         self._login = login
         self._password = password
         self._nick = nick
-        self._db.cur.execute('insert into Account (login, password, nick, state) values (%s, %s, %s, %s);',
-                            (self._login, self._passw.get_hashed_password(), self._fname, self._lname))
+        self._db.cur.execute('insert into "Account" (login, password, nick, state) values (%s, %s, %s, %s);',
+                            (self._login, self._password, self._nick, self._state))
         self._db.con.commit()
 
 # Class represents logges user
 class Logged_user(User):
-    def __init__(self, login: str, state: State) -> None:
+    def __init__(self, login: str, state: State, db: Database_connection) -> None:
         self._login = login
         self._state = state
+        self._db = db
 
     def get_login(self) -> str:
         return self._login
@@ -176,7 +194,7 @@ class Logged_user(User):
         return
 
 
-class Car:
+class Car():
     def __init__(self, db: Database_connection) -> None:
         self._db = db
 
@@ -188,12 +206,14 @@ class Car:
             (brand, model, login))
         self._db.con.commit()
 
+
     def add_car_to_db(self, brand, model) -> None:
         self._db.cur.execute('insert into Car (brand, model) values (%s, %s);',
                              (brand, model))
         self._db.con.commit()
 
     def change_car(self, brand, model, login) -> None:
+
         self._db.cur.execute(
             'UPDATE "Account" '
             'SET id_car = (Select id_car FROM "Car" WHERE brand = (%s) and model = (%s)) '
@@ -209,11 +229,11 @@ class Proxy():
         while(True):
             login = str(input("Login: "))
             password = str(input("Password: "))
-            if(User_exist(login).check()):
-                if(Login(login, password).sign()):
+            if(User_exist(login, self._db).check()):
+                if(Login(login, password, self._db).sign()):
                     print("Login successfull!")
                     Clear_terminal()
-                    return Logged_user(login, Login(login, password).get_state())
+                    return Logged_user(login, Login(login, password, self._db).get_state(), self._db)
                 else:
                     print("Wrong password!")
             else:
@@ -225,11 +245,11 @@ class Proxy():
             login = str(input("Login: "))
             password = str(input("Password: "))
             nick = str(input("Nick: "))
-            if(not User_exist(login).check()):
-                if(not Nick_exist(nick).check()):
-                    if(Password_check.check(password)):
+            if(not User_exist(login, self._db).check()):
+                if(not Nick_exist(nick, self._db).check()):
+                    if(Password_check().check(password)):
                         password = Hash(password).get_hashed_password()
-                        Register.register(login, password, nick)
+                        Register(self._db).register(login, password, nick)
                         print("Registered successfully!")
                         Clear_terminal()
                         return
@@ -248,8 +268,8 @@ class Proxy():
         self._db.cur.execute('select count(*) from "Car" where brand = %s and model = %s;', (brand, model))
         in_base = self._db.cur.fetchone()
         if (in_base[0] == 1):
-            Car.add_car_to_user(brand, model, user.get_login())
-            print("Car added successfully!")
+            Car(self._db).add_car_to_user(brand, model, user.get_login())
+            print("Car added/changed successfully!")
             input("\nPress Enter to continue...")
             os.system('cls' if os.name == 'nt' else 'clear')
             return
@@ -264,7 +284,7 @@ class Proxy():
         self._db.cur.execute('select count(*) from "Car" where brand = %s and model = %s;', (brand, model))
         in_base = self._db.cur.fetchone()
         if (in_base[0] != 0):
-            Car.add_car_to_db(brand, model)
+            Car(self._db).add_car_to_db(brand, model)
             print("Car added successfully!")
             input("\nPress Enter to continue...")
             os.system('cls' if os.name == 'nt' else 'clear')
@@ -280,7 +300,7 @@ class Proxy():
         self._db.cur.execute('select count(*) from "Car" where brand = %s and model = %s;', (brand, model))
         in_base = self._db.cur.fetchone()
         if (in_base[0] == 1):
-            Car.change_car(brand, model, user.get_login())
+            Car(self._db).change_car(brand, model, user.get_login())
             print("Car added successfully!")
             input("\nPress Enter to continue...")
             os.system('cls' if os.name == 'nt' else 'clear')
@@ -309,7 +329,7 @@ class Proxy():
         pass
 
 
-class View:
+class View:#TODO dodac zobaczenie jakie auta sa i lokalizacje
     def __init__(self, db):
         self._db = db
 
@@ -320,19 +340,108 @@ class View:
         pass
 
 class Login_menu:
-    pass
+    def __init__(self, db: Database_connection) -> None:
+        self.choice = ""
+        self._db = db
+
+    def start(self) -> None:
+        print("LOGIN MENU")
+        while True:
+            self.choice = int(input("""
+            Choose an option:
+            1. Login
+            2. Register
+            0. Exit
+            """))
+            if self.choice == 1:
+                user = Proxy(self._db).login_proxy()
+                if user.get_state() is Meet_state:
+                    Meet_menu(user, self._db).start()
+                    break;
+                else:
+                    No_meet_menu(user, self._db).start()
+                    break;
+            elif self.choice == 2:
+                Proxy(self._db).register_proxy()
+            elif self.choice == 0:
+                print("Exiting program...")
+                Clear_terminal()
+                break;
 
 class User_settings:
-    pass
+    def __init__(self, user: Logged_user, db: Database_connection) -> None:
+        self._user = user
+        self._db = db
+        self.choice = ""
+
+    def start(self) -> None:
+        print("Logged user menu")
+        while True:
+            self.choice = int(input("""
+            Choose an option:
+            1. Change state
+            2. Add/change your car
+            3. Change password
+            4. Change nick
+            5. Add car to database
+            0. Exit
+            """))
+            if self.choice == 1:
+                pass
+            elif self.choice == 2:
+                Proxy(self._db).add_car_to_user_proxy(self._user)
+            elif self.choice == 3:
+                Proxy(self._db).change_password_proxy(self._user)
+            elif self.choice == 4:
+                Proxy(self._db).change_nick_proxy(self._user)
+            elif self.choice == 5:
+                Proxy(self._db).add_car_to_db_proxy()
+            elif self.choice == 0:
+                print("Exiting program...")
+                Clear_terminal()
+                break
+            else:
+                print("Wrong option!")
 
 class Meet_menu:
-    pass
+    def __init__(self, user: Logged_user, db: Database_connection) -> None:
+        self._db = db
+        self._user = user
+    def start(self):
+        print("LOGIN MENU")
+        while True:
+            self.choice = int(input("""
+                    Choose an option:
+                    1. Meet
+                    2. View locations
+                    3. View cars
+                    4. Settings
+                    0. Exit
+                    """))
+            if self.choice == 1:
+                pass
+            elif self.choice == 2:
+                pass
+            elif self.choice == 3:
+                pass
+            elif self.choice == 4:
+                User_settings(self._user, self._db).start()
+            elif self.choice == 0:
+                print("Exiting program...")
+                Clear_terminal()
+                break;
 
 class No_meet_menu:
-    pass
+    def __init__(self, db: Database_connection) -> None:
+        self._db = db
+
+    def start(self):
+        print("No meet state")
+
 
 def main():
     db = Database_connection()
+    Login_menu(db).start()
 
 if __name__ == "__main__":
     main()
